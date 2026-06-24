4e61576561f3248612d679a5bdded7fa10ca0855
Amodh Dhakal <140471489+amodhakal@users.noreply.github.com>
2026-06-24 12:34:04 -0400

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,45 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        DIRS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        def dfs(rowidx, colidx, chidx, visit):
+            # Done
+            if chidx >= len(word):
+                return True
+
+            # Out of bounds
+            if rowidx < 0 or rowidx >= ROWS or colidx < 0 or colidx >= COLS:
+                return False
+            
+            # Already used
+            if (rowidx, colidx) in visit:
+                return False
+            
+            # Can't be used
+            if board[rowidx][colidx] != word[chidx]:
+                return False
+
+
+
+            # Try completing word
+            for diffX, diffY in DIRS:
+                # Can be used
+                visit.add((rowidx, colidx))
+
+                if dfs(rowidx + diffX, colidx + diffY, chidx + 1, visit):
+                    return True
+
+                visit.remove((rowidx, colidx))
+
+            return False
+
+        for rowidx in range(ROWS):
+            for colidx in range(COLS):
+                currset = set()
+                
+                if dfs(rowidx, colidx, 0, currset):
+                    return True
+
+        return False
+
