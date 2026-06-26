b80667f8974f4ef036ce4a85d39a3a799091204a
Amodh Dhakal <140471489+amodhakal@users.noreply.github.com>
2026-06-26 11:38:55 -0400

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-2

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        largest_num = max(nums)
+        l, r = 0, len(nums)
+
+        while l <= r:
+            x = (l + r) // 2
+            result = 0
+            for num in nums:
+                if num >= x:
+                    result += 1
+            
+            if result == x:
+                return x
+
+            if result < x:
+                r = x - 1
+            
+            if result > x:
+                l = x + 1
+
+        return -1
