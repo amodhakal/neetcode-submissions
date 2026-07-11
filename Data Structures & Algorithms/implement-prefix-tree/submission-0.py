f167c8e9ef825f56e5672a19825285f0dd505de9
Amodh Dhakal <140471489+amodhakal@users.noreply.github.com>
2026-07-11 10:49:37 -0400

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+@dataclass
+class PrefixNode:
+    ch: str
+    children: dict
+    is_end: bool
+
+    def __str__(self):
+        return f"{self.ch} {"DONE" if self.is_end else ""}"
+
+class PrefixTree:
+    def __init__(self):
+        self.root = PrefixNode("DUMMY", defaultdict(PrefixNode), False)
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                new_node = PrefixNode(ch, defaultdict(PrefixNode), False)
+                curr.children[ch] = new_node
+
+            curr = curr.children[ch]
+
+        curr.is_end = True
+        print(self.root)
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            curr = curr.children[ch]
+            if not curr:
+                return False
+
+        return curr.is_end
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            curr = curr.children[ch]
+            if not curr:
+                return False
+
+        return True
+        
+        
